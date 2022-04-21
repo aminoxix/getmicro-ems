14c546c6f36278596f22fd7207cb3e54a6657f4a
Pratik Aswani <pratikaswani@Pratiks-MacBook-Air.local>
2022-04-21 17:18:55 +0530

    Signin auth completed

--- attendence-system-app/src/Pages/SignIn/Signin.jsx
+++ attendence-system-app/src/Pages/SignIn/Signin.jsx
@@ -1,4 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
+import { app } from '../../Config/firebase-config';
 
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -11,6 +17,10 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -36,21 +46,71 @@ const theme = createTheme();
 
 export default function Signin() {
   const [open, setOpen] = useState(false);
+  const [showSnackBar, setShowSnackBar] = useState(false);
+  const [message, setMessage] = useState();
+
+  const authentication = getAuth();
+  let navigate = useNavigate();
+
+
+
+  const handleShowSnackBar = () => {
+    setShowSnackBar(true);
+  };
+
+  const handleCloseSnackBar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setShowSnackBar(false);
+  };
+
+  const closeBackDrop = () => {
+    setOpen(false);
+  }
 
   const handleToggle = () => {
     setOpen(!open);
   };
 
+  
 
   const handleSubmit = (event) => {
     event.preventDefault();
     handleToggle();
+    
     const data = new FormData(event.currentTarget);
+    signInWithEmailAndPassword(authentication, data.get('email'), data.get('password'))
+      .then((response) => {
+        console.log(response);
+        closeBackDrop();
+        setMessage("Signed in successfully");
+        handleShowSnackBar();
+        navigate('/dashboard')
+        sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken);
+      }).catch((error) => {
+        setMessage(`${error}`);
+        closeBackDrop();
+        handleShowSnackBar();
+      })
     console.log({
       email: data.get('email'),
       password: data.get('password'),
     });
   };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleCloseSnackBar}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
 
   useEffect(() => {
       document.title = "Signin";
@@ -95,10 +155,6 @@ export default function Signin() {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
@@ -111,6 +167,13 @@ export default function Signin() {
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
+      <Snackbar
+        open={showSnackBar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackBar}
+        message={message}
+        action={action}
+      />
        <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={open}
